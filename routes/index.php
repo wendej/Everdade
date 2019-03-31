23ab2dde324a8f5835287b900b77f3b80fdb7f59
Wenderson Jr <wendersonej.74@gmail.com>
2019-03-31 17:52:28 -0300

    liberação da rota localhost/3000

--- routes/index.php
+++ routes/index.php
@@ -1,7 +1,8 @@
 <?php 
 
 use function src\{
-    slimConfiguration
+    slimConfiguration,
+    basicAuth
 };
 
 use App\Controllers\CursoController;
@@ -9,14 +10,27 @@ use App\Controllers\UsuarioController;
 
 $app = new \Slim\App(slimConfiguration());
 
-// ==================================================
-$app->get('/cursos', CursoController::class . ':getCursos');
+$app
+->group('', function() use ($app) {
+	$app->get('/cursos', CursoController::class . ':getCursos');
+	$app->get('/usuario/seleciona', UsuarioController::class . ':getUsuario');
+	$app->post('/usuario/cadastro', UsuarioController::class . ':insertUsuario');
+	$app->put('/usuario/atualizar', UsuarioController::class . ':updateUsuario');
+	$app->delete('/usuario/apagar', UsuarioController::class . ':deleteUsuario');
+	$app->post('/usuario/login', UsuarioController::class . ':loginUsuario');
+})
+->add(function ($req, $res, $next) {
+    $response = $next($req, $res);
+    return $response
+            ->withHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+})
+->add(basicAuth());
 
-$app->get('/usuario/seleciona', UsuarioController::class . ':getUsuario');
-$app->post('/usuario/cadastro', UsuarioController::class . ':insertUsuario');
-$app->put('/usuario/atualizar', UsuarioController::class . ':updateUsuario');
-$app->delete('/usuario/apagar', UsuarioController::class . ':deleteUsuario');
-$app->post('/usuario/login', UsuarioController::class . ':loginUsuario');
-// ==================================================
+$app->map(['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], '/{routes:.+}', function($req, $res) {
+    $handler = $this->notFoundHandler; // handle using the default Slim page not found handler
+    return $handler($req, $res);
+});
 
 $app->run();
